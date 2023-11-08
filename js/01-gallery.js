777bd2e3e529aa06a069fdcc47b787da5955b1f1
IhorRatsyn <ratsinjob@gmail.com>
2023-11-08 23:08:35 +0200

    213

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,51 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import throttle from 'lodash.throttle';
+import SimpleLightbox from 'simplelightbox'; // Імпортуємо SimpleLightbox
+import 'simplelightbox/dist/simple-lightbox.min.css'; // Імпорт стилів бібліотеки
 
-console.log(galleryItems);
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const feedbackKey = 'feedback-form-state';
+
+const saveToLocalStorage = throttle(() => {
+  const data = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.setItem(feedbackKey, JSON.stringify(data));
+}, 500);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const storedData = localStorage.getItem(feedbackKey);
+  if (storedData) {
+    const { email, message } = JSON.parse(storedData);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+});
+
+form.addEventListener('input', saveToLocalStorage);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const dataToSubmit = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+
+  emailInput.value = '';
+  messageInput.value = '';
+  localStorage.removeItem(feedbackKey);
+
+  console.log('Submitted Data:', dataToSubmit);
+});
+
+// Після додавання вмісту галереї (як приклад)
+const images = [
+  { src: 'image1.jpg' },
+  { src: 'image2.jpg' },
+  { src: 'image3.jpg' }
+];
+
+const lightbox = new SimpleLightbox(images);
--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player(document.getElementById('vimeo-player'));
+
+player.on('timeupdate', throttle(async function(event) {
+  const currentTime = await player.getCurrentTime();
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000)); // оновлення не частіше, ніж раз на секунду
+
+// Встановлення часу відтворення після перезавантаження сторінки
+document.addEventListener('DOMContentLoaded', async function() {
+  const storedTime = localStorage.getItem('videoplayer-current-time');
+  if (storedTime !== null) {
+    await player.setCurrentTime(parseFloat(storedTime));
+  }
+});
