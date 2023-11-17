cf159b574336ae386b41ae0f31301a0692474fa7
IhorRatsyn <ratsinjob@gmail.com>
2023-11-17 23:19:57 +0200

    21

--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,20 +1,18 @@
-import throttle from 'lodash.throttle';
+import throttle from 'lodash/throttle'; // Змінила шлях для lodash
 
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 const feedbackKey = 'feedback-form-state';
 
-// Функція для зберігання даних у локальне сховище
 const saveToLocalStorage = throttle(() => {
   const data = {
     email: emailInput.value,
     message: messageInput.value
   };
   localStorage.setItem(feedbackKey, JSON.stringify(data));
-}, 500); // Оновлення не частіше, ніж раз на 500 мілісекунд
+}, 500);
 
-// Перевірка стану сховища при завантаженні сторінки
 document.addEventListener('DOMContentLoaded', () => {
   const storedData = localStorage.getItem(feedbackKey);
   if (storedData) {
@@ -24,24 +22,20 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-// Відстеження події input і зберігання даних у локальне сховище
 form.addEventListener('input', saveToLocalStorage);
 
-// Обробка події сабміту форми
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  
+
   const dataToSubmit = {
     email: emailInput.value,
     message: messageInput.value
   };
 
-  // Очищення полів форми та сховища
   emailInput.value = '';
   messageInput.value = '';
   localStorage.removeItem(feedbackKey);
 
-  // Виведення даних у консоль
   console.log('Submitted Data:', dataToSubmit);
 });
 
