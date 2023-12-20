025c35e8f5c9e1a039c10f499afdd0e5c494574b
IhorRatsyn <ratsinjob@gmail.com>
2023-12-20 21:18:40 +0200

    fixed some bugs

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,31 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-document.addEventListener("DOMContentLoaded", function () {
-  const gallery = new SimpleLightbox('.gallery a', {});
-});
-
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.querySelector('.gallery');
+
+  function createGalleryItem(item) {
+    const li = document.createElement('li');
+    li.classList.add('gallery__item');
+
+    const a = document.createElement('a');
+    a.classList.add('gallery__link');
+    a.href = item.original;
+
+    const img = document.createElement('img');
+    img.classList.add('gallery__image');
+    img.src = item.preview;
+    img.dataset.source = item.original;
+    img.alt = item.description;
+
+    a.appendChild(img);
+    li.appendChild(a);
+    return li;
+  }
+
+  const galleryItemsHTML = galleryItems.map(createGalleryItem);
+  gallery.append(...galleryItemsHTML);
+
+   new SimpleLightbox('.gallery__link', {});
+});
